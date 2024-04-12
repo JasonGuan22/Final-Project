618711661b6d73cfd8609b930c929ec1fc6fb1c3
Zayd Mahfuz <zmahfuz@terpmail.umd.edu>
2024-04-12 16:10:44 -0400

    Added function that gives nutritional values.

--- Workout_code.py
+++ Workout_code.py
@@ -3,41 +3,66 @@ from argparse import ArgumentParser
 
 
 class WorkoutPlanner:
-    
+
     def __init__(self):
         self.workout = {
             "Ectomorph": ["Bench Press", "Pull Ups", "Deadlifts", "Squats"],
             "Mesomorph": ["Pushups", "Burpee", "Mountain Climbers", "Lunges"],
-            "Endomorph": ["Treadmill Sprint", "Barbell Back Squat", 
+            "Endomorph": ["Treadmill Sprint", "Barbell Back Squat",
                           "Battling Ropes", "Indoor Cycling"],
         }
-    
+
     def get_body(self):
         while True:
-            body_type = input("Please enter your body type (Ectomorph, Mesomorph, Endomorph)")
+            body_type = input("Please enter your body type (Ectomorph, Mesomorph, Endomorph) ")
             if body_type in self.workout:
+                self.body_type = body_type
                 return body_type
-            else: 
+            else:
                 print("Invalid body type. Please enter either Ectomorph, Mesomorph, Endomorph")
-            
+
     def workout_routine(self):
-        body_type = self.get_body()
-        if body_type in self.workout:
-            return self.workout[body_type]
         
-    
-    
+        if self.body_type in self.workout:
+            return self.workout[self.body_type]
+
+
+class TargetNutrition(WorkoutPlanner):
+
+    def __init__(self):
+        super().__init__() 
+        self.targets = {  
+            "cut": ["Protein: 60g", "Carbs: 40g", "Fat: 30g"],
+            "bulk": ["Protein: 70g", "Carbs: 60g", "Fat: 40g"],
+        }
+
+    def get_cut_bulk_goal(self):
+        while True:
+            goal = input("Are you looking to cut or bulk (cut/bulk): ")
+            if goal.lower() in self.targets:
+                return goal.lower()
+            else:
+                print("Invalid goal. Please enter either cut or bulk")
+
+    def get_nutritional_targets(self):
         
-        
-        
-        
-    
-    
-    
- 
-        
-        
-    
-    
-    
-    
+        self.body_type = self.get_body()
+        cut_bulk_goal = self.get_cut_bulk_goal()
+        workout_routine = super().workout_routine()  
+
+        target_plan = {
+            "body_type": self.body_type,
+            "workout_routine": workout_routine,
+            "cut_bulk": cut_bulk_goal,
+            "nutritional_targets": self.targets[cut_bulk_goal]
+        }
+        return target_plan
+
+
+target_nutrition = TargetNutrition()
+nutritional_plan = target_nutrition.get_nutritional_targets()
+print(f"Based on your body type ({nutritional_plan['body_type']}) and goal({nutritional_plan['cut_bulk']}):")
+print(f"Workout Routine: {nutritional_plan['workout_routine']}")
+print(f"Target Nutrients:")
+for target in nutritional_plan["nutritional_targets"]:
+    print(f"\t{target}")
