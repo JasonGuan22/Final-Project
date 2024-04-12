6b0fa281798e5089e49102e9c2e3103eb12b2b3b
Jason Guan <Jasong22@terpmail.umd.edu>
2024-04-12 16:19:11 -0400

    "added regular expression"

--- Workout_code.py
+++ Workout_code.py
@@ -1,12 +1,13 @@
-import json
 from argparse import ArgumentParser
-
+import re
+import json
 
 class WorkoutPlanner:
     
     def __init__(self):
         self.workout = {
-            "Ectomorph": ["Bench Press", "Pull Ups", "Deadlifts", "Squats"],
+            "Ectomorph": ["Bench Press", "Pull Ups", "Deadlifts", "Squats", 
+                          "Standing Military Press", "Compound Lifts"],
             "Mesomorph": ["Pushups", "Burpee", "Mountain Climbers", "Lunges"],
             "Endomorph": ["Treadmill Sprint", "Barbell Back Squat", 
                           "Battling Ropes", "Indoor Cycling"],
@@ -23,10 +24,37 @@ class WorkoutPlanner:
     def workout_routine(self):
         body_type = self.get_body()
         if body_type in self.workout:
-            return self.workout[body_type]
+            routine = self.workout[body_type]
+            return routine 
         
+         
+    
+
     
     
+    
+    
+
+    def get_body(self):
+        pattern = r"^\s*(Ectomorph|Mesomorph|Endomorph)\s*$"
+        while True:
+            body_type = input("Please enter your body type (Ectomorph, Mesomorph, Endomorph)")
+            match = re.match(pattern, body_type, re.IGNORECASE)  
+            if match:
+                return match.group(1)
+            else: 
+                print("Invalid body type. Please enter either Ectomorph, Mesomorph, Endomorph")
+              
+    def workout_routine(self, body_type = "Mesomorph"):
+        body_type = self.get_body()
+        if body_type in self.workout:
+            routine = self.workout[body_type]
+            print(f"This is your recommended workout routine {routine}.")
+            
+            
+            
+
+
         
         
         
